caf9ecc03e6cd22be049d2f9bf136d586922ad61
Felipe Negreiros <142634879+Dev-felipe02@users.noreply.github.com>
2025-04-07 22:57:00 -0300

    Add files via upload

---
+++ javascript/scriptfaccat06.3.js
@@ -0,0 +1,5 @@
+alert("Base Triangulo");
+let base = parseFloat(prompt("Digite o valor da base: "));
+let altura = parseFloat(prompt("Digite o valor da altura: "))
+let area = base * altura / 2 ;
+alert("O valor da area é: "+ area);
