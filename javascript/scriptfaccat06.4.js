1d87374f891bb3132dec51478156ec0cfcebda21
Felipe Negreiros <142634879+Dev-felipe02@users.noreply.github.com>
2025-04-07 23:35:47 -0300

    Add files via upload

---
+++ javascript/scriptfaccat06.4.js
@@ -0,0 +1,5 @@
+alert("Base Losango");
+let D = parseFloat(prompt("Digite o valor da diagonal maior: "));
+let d = parseFloat(prompt("Digite o valor da diagonal menor: "));
+let area = D * d / 2 ;
+alert("O valor da area é: "+ area);
