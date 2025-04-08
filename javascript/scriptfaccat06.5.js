058397aae28de68233489aa3e513adaa8608aa2f
Felipe Negreiros <142634879+Dev-felipe02@users.noreply.github.com>
2025-04-07 23:52:03 -0300

    Add files via upload

---
+++ javascript/scriptfaccat06.5.js
@@ -0,0 +1,6 @@
+alert("Base Trapézio");
+let B = parseFloat(prompt("Digite o valor da base maior: "));
+let b = parseFloat(prompt("Digite o valor da base menor: "));
+let altura = parseFloat(prompt("Digite o valor da altura: "));
+let area = ((B + b) * altura) / 2 ;
+alert("O valor da area é: "+ area);
