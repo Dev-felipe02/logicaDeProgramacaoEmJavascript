1a2f4081872f62d7909a84b0696213487b423f0f
Felipe Negreiros <142634879+Dev-felipe02@users.noreply.github.com>
2025-04-09 18:48:27 -0300

    Add files via upload

---
+++ javascript/scriptfaccat20.js
@@ -0,0 +1,11 @@
+alert("Ordem crescente.");
+
+let num01 = parseFloat(prompt("Digite um numero: "));
+let num02 = parseFloat(prompt("Digite um segundo numero: "));
+
+if (num01 > num02) {
+    alert("A ordem crescente dos numeros digitados é: "+ num02 +" "+ num01);
+}
+else {
+    alert("A ordem crescente dos numeros digitados é: "+ num01 +" "+ num02);
+}
