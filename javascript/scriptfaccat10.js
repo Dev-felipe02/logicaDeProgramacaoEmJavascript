655cf96c1098eed914d4527099efa570ac150e0c
Felipe Negreiros <negreirosfelipe337@gmail.com>
2025-04-09 14:32:07 -0300

    Update scriptfaccat10.js

--- javascript/scriptfaccat10.js
+++ javascript/scriptfaccat10.js
@@ -1,4 +1,4 @@
 alert("Culto final de carro.");
 let carro = parseFloat(prompt("Digite o valor do carro: "));
 let final = (28 / 100 * carro) + (45 / 100 * carro) + carro ; 
-alert("O valor final do carro para o cliente é: R$"+ final)
+alert("O valor final do carro para o cliente é: R$"+ final);
