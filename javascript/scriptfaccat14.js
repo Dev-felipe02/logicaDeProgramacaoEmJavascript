e8e673d34e831681edd54f3e8b44358bdf6c1cf8
Felipe Negreiros <142634879+Dev-felipe02@users.noreply.github.com>
2025-04-09 18:04:39 -0300

    Add files via upload

---
+++ javascript/scriptfaccat14.js
@@ -0,0 +1,10 @@
+alert("Maior ou Menor que 10.");
+
+let num = parseInt(prompt("Digite um numero: "));
+
+if (num > 10) {
+    alert("Numero digitado é maior que 10.");
+}
+else {
+    alert("Numero digitado não é maior que 10.");
+}
