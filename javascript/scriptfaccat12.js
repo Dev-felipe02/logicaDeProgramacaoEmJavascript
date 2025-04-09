222e2b1b65bffaef77cef367fb6f94378a4ad2b2
Felipe Negreiros <142634879+Dev-felipe02@users.noreply.github.com>
2025-04-09 17:43:36 -0300

    Add files via upload

---
+++ javascript/scriptfaccat12.js
@@ -0,0 +1,6 @@
+alert("Fahrenheit para Celsius.");
+
+let F = parseFloat(prompt("Digite uma temperatura em Fahrenheit: "));
+let C = (F - 32) / 1.8
+
+alert("A temperatu em graus Celsius é: "+ C +"°C");
