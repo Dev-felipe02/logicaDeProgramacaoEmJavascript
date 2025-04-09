46db46255aa5e7e78b9196cd5382fd3ba83ab4c7
Felipe Negreiros <142634879+Dev-felipe02@users.noreply.github.com>
2025-04-09 18:09:26 -0300

    Add files via upload

---
+++ javascript/scriptfaccat15.js
@@ -0,0 +1,10 @@
+alert("Numero possitivo ou negativo.");
+
+let num = parseInt(prompt("Digite um numero: "));
+
+if (num > -1) {
+    alert("Numero digitado é possitivo.")
+}
+else {
+    alert("Numero digitado é negativo.")
+}
