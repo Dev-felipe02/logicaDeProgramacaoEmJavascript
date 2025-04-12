9bd8fcf736a31d2e695857ec303c5f98f6737195
Felipe Negreiros <142634879+Dev-felipe02@users.noreply.github.com>
2025-04-12 14:00:24 -0300

    scriptexercicio07.js

--- javascript/scriptexercicio07.js
+++ javascript/scriptexercicio07.js
@@ -1,12 +1,13 @@
 alert("Adivinhe o número");
 
 let senha = parseInt(prompt("Digite a senha correta: "));
+let num_aleatorio = Math.floor(Math.random() * 10) + 1 ;
 
-if (senha != 1234) {
+if (senha != num_aleatorio) {
 do {
     alert("Senha incorreta!")
     senha = parseInt(prompt("Digite a senha correta: "));
-} while (senha != 1234)
+} while (senha != num_aleatorio)
     alert("Senha correta!")
 }
 else {
