8b70139ef4c3dd167eb69be0d138791d26d5c080
Felipe Negreiros <142634879+Dev-felipe02@users.noreply.github.com>
2025-04-12 17:41:31 -0300

    Add files via upload

---
+++ javascript/scriptL01L.js
@@ -0,0 +1,13 @@
+alert("Soma dos quadrados de A, B e C.");
+
+let A = parseInt(prompt("Digite um valor para A: "));
+let B = parseInt(prompt("Digite um valor para B: "));
+let C = parseInt(prompt("Digite um valor para C: "));
+
+let quadradoA = A ** 2 ;
+let quadradoB = B ** 2 ;
+let quadradoC = C ** 2 ;
+
+let soma = quadradoA + quadradoB + quadradoC ;
+
+alert("A soma dos quadrados de A, B e C é: "+ soma);
---
+++ javascript/scriptL01M.js
@@ -0,0 +1,10 @@
+alert("Quadrado da soma de A, B e C.");
+
+let A = parseInt(prompt("Digite um valor para A: "));
+let B = parseInt(prompt("Digite um valor para B: "));
+let C = parseInt(prompt("Digite um valor para C: "));
+
+let soma = A + B + C ;
+let quadrado = soma ** 2 ;
+
+alert("A soma dos quadrados de A, B e C é: "+ quadrado);
