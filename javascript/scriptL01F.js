6aaf94823d12bc655a146aec47cbfa09397eb4bb
Felipe Negreiros <142634879+Dev-felipe02@users.noreply.github.com>
2025-04-12 15:43:54 -0300

    Add files via upload

---
+++ javascript/scriptL01F.js
@@ -0,0 +1,9 @@
+alert("Troca de valores.");
+
+let A = parseInt(prompt("Digite um valor: "));
+let B = parseInt(prompt("Digite um valor: "));
+
+let A02 = B ;
+let B02 = A ;
+
+alert("O valor de A passou a ser "+ A02 + ", e o valor de B passou a ser "+ B02 + ".");
