b266c3a2927c49841e1fff0245081cf7ff25c9c4
Felipe Negreiros <142634879+Dev-felipe02@users.noreply.github.com>
2025-04-12 18:08:38 -0300

    Add files via upload

---
+++ javascript/scriptL0A01.js
@@ -0,0 +1,11 @@
+alert("L01A01.");
+
+let A = parseInt(prompt("Digite o valor de A:"));
+let B = parseInt(prompt("Digite o valor de B:"));
+let C = parseInt(prompt("Digite o valor de C:"));
+let D = parseInt(prompt("Digite o valor de D:"));
+
+let produto = A * C;
+let soma = B + D;
+
+alert("O valor do produto de A com C é: "+ produto + ", e a soma de B com D é: "+ soma);
