41bc9ba7093b99644250baebd55f2b36715a45e9
Felipe Negreiros <142634879+Dev-felipe02@users.noreply.github.com>
2025-04-13 15:38:17 -0300

    Add files via upload

---
+++ javascript/scriptL03B.js
@@ -0,0 +1,14 @@
+alert("L03BA.");
+
+let soma = 1 ;
+let contador01 = 2 ;
+let contador02 = 1 ;
+
+
+while (contador02 < 100) {
+    soma = soma + contador01 ;
+    contador01 ++ ;
+    contador02 ++ ;
+}
+
+alert(soma);
