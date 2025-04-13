2ffcecca219f1288dea7749a844d5c1491c05a69
Felipe Negreiros <142634879+Dev-felipe02@users.noreply.github.com>
2025-04-13 02:18:35 -0300

    Add files via upload

---
+++ javascript/scriptL02J.js
@@ -0,0 +1,10 @@
+alert("L02J.");
+
+let num = parseInt(prompt("Digite um numero: "));
+
+if (num > 0 && num < 10) {
+    alert("O valor está na faixa permitida.");
+}
+else {
+    alert("O valor está fora da faixa permitida.");
+}
