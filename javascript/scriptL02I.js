79aa59f0e066a22f87bbc5c3ee082fd392295372
Felipe Negreiros <142634879+Dev-felipe02@users.noreply.github.com>
2025-04-13 02:13:19 -0300

    Add files via upload

---
+++ javascript/scriptL02I.js
@@ -0,0 +1,10 @@
+alert("L02I.");
+
+let num = parseInt(prompt("Digite um numero: "));
+
+if (num % 2 === 0){
+    alert(`O numero ${num} é par.`);
+}
+else {
+    alert(`O numero ${num} é impar.`);
+}
