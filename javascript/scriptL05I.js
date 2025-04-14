7bb85de6c6da1e72d29af36730b3de4ddca78fa7
Felipe Negreiros <142634879+Dev-felipe02@users.noreply.github.com>
2025-04-14 19:25:17 -0300

    scriptL05I.js

--- javascript/scriptL05I.js
+++ javascript/scriptL05I.js
@@ -2,15 +2,11 @@ alert("L05I.");
 
 let fibonacci = 1 ;
 let resultado = 1 ;
-//let contador = 2 ;
 
 console.log(`${resultado}`);
 
-//while (contador < 16) {
 for (let contador = 2 ; contador < 16 ; contador ++) {
     console.log(`${resultado}`);
     resultado = resultado + fibonacci ;
     fibonacci = resultado - fibonacci ;
 }
-    //contador ++ ;
-//}
